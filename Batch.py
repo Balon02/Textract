5a0e893abe74b712d4f595a3662bf9c3732e5c05
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-05-22 22:51:17 +0200

    made final touches for alpha version

--- Batch.py
+++
@@ -1,9 +0,0 @@
-import os
-from Page import Page
-
-class Batch:
-    def __init__(self, path):
-        self.path = path
-        self.pages = []
-        self.output_folder = os.path.join(self.path, 'output_texts')
-        os.makedirs(self.output_folder, exist_ok=True)
