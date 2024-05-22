4d7499159994633d2c5b6f5c1bcf427ad8a38765
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-05-22 22:47:11 +0200

    made final touches for alpha version

--- gui.py
+++ gui.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import customtkinter as ctk
 import pyperclip
 import keyboard
-import time
 from Page import clipboard_to_string
+from pynput import mouse
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -21,24 +21,25 @@ class App(ctk.CTk):
         self.button = ctk.CTkButton(self, text="Copy To Clipboard", command=self.copy_to_clipboard)
         self.button.grid(row=1, column=0, padx=20, pady=20)
 
-        keyboard.on_press_key("Print Screen", self.shortcut_press)
+        keyboard.add_hotkey("print screen", lambda: keyboard.send("win+shift+s"))
+        self.mouse_listener = mouse.Listener(on_click=self.mouse_press)
+        self.mouse_listener.start()
+
 
     def copy_to_clipboard(self):
         pyperclip.copy(self.text_var.get())
         print("Copied to clipboard")
 
-    def shortcut_press(self, event):
-        try:
-            time.sleep(5)
-            result_text = clipboard_to_string()
-            self.text_var.set(result_text)
-            self.textbox.delete("0.0", tk.END)
-            self.textbox.insert("0.0", f"{self.text_var.get()}")
-        except Exception as e:
-            print("Failed to capture screenshot")
-    def remap_key(self):
-        keyboard.remap_key(44, "win+shift+s")
-        keyboard.wait()
+    def mouse_press(self, x, y, button, pressed):
+        if button == mouse.Button.left and not pressed:
+            try:
+                result_text = clipboard_to_string()
+                self.text_var.set(result_text)
+                self.textbox.delete("0.0", tk.END)
+                self.textbox.insert("0.0", f"{self.text_var.get()}")
+            except Exception as e:
+                print("Failed to capture screenshot")
+
 
 def main():
     app = App()
