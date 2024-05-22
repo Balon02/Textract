8728502ace5bd197f6038295d1ff6be5f032bb44
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-05-22 23:03:45 +0200

    made final touches for alpha version

--- Page.py
+++ Page.py
@@ -7,7 +7,7 @@ import pyperclip
 from PIL import ImageGrab
 
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\chope\Desktop\Tesseract\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r''
 
 
 class Page:
